13dd85a53c4c29605ea19cd447b14421804b115c
MarcoChiantia <98151014+MarcoChiantia@users.noreply.github.com>
2022-02-11 09:58:59 +0100

    EsercizioStringheApp2

---
+++ ChiantiaEsercizio1/ChiantiaEsercizio1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ChiantiaEsercizio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string parola = "  ";
+            int cc = 0;
+            int d1 = 0;
+            int sommac = 0;
+            int somma1 = 0;
+            int mediac = 0;
+            int medIA1 = 0;
+
+            while (parola != "fine") ;
+            {
+                Console.WriteLine("inserisci una parola");
+                parola = Console.ReadLine();
+                int lungh = parola.Length;
+                if (lungh < 0) ;
+                {
+                    cc++
+                        sommac++;
+                }
+                else if (lungh <= 0)
+                {
+                    c1++
+                }
+            }
+    }
+}
